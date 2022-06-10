6bbc92861f5147b2d4238561e1bcc55cab8a0044
people called me <Starknntnk99@gmail.com>
2022-06-10 20:51:28 +0700

    new

--- c/basic/headers/questionHeader/TypeData.hpp
+++ c/basic/headers/questionHeader/TypeData.hpp
@@ -2,11 +2,17 @@
 #include <errno.h>
 #include <typeinfo>
 
-int TypeData() {
-    std::string iniString = "stringg";
-    std::cout << typeid(iniString).name();
+struct animal {
+	std::string nama;
+	int umur;
+}animals;
 
-    
+int TypeData() {
+	std::cout << "tambahkan hewan" << std::endl;
+	std::getline(std::cin, animals.nama);
+
+	std::cout << animals.nama << std::endl;
+
     std::cin.get();
 
     return 0;
